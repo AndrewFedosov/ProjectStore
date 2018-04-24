d31f5ea2fa4e5bc06bb21ccfc6b311a3f18c2474
DESKTOP-V9VV0TS\Andrei Fedosov <w.walenor.w@gmail.com>
2018-04-24 10:35:12 +0300

    finish

--- landing/urls.py
+++ landing/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from landing import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^landing123/$', views.landing, name='landing'),
+    url(r'^$', views.landing, name='landing'),
+    url(r'^home$', views.home, name='home'),
 ]
